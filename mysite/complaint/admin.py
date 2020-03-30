7dbc127e6b25acdc0140371517d7864c906de062
khan0604 <khansohail0604@gmail.com>
2020-03-31 01:36:02 +0530

    admin code written

--- mysite/complaint/admin.py
+++ mysite/complaint/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Complaint
 
 # Register your models here.
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    class Meta :
+        model = Complaint
